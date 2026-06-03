3239dc3854d28d541a3de5aa5db9fe9461f07339
Daosolary <adsubagi@gmail.com>
2026-06-03 09:23:20 +0000

    novo git

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Footer } from "@/components/Footer";
 import { ToastProvider } from "@/components/ui/ToastProvider";
 
 export const metadata: Metadata = {
-  title: "Repositorio",
+  title: "Repositorio Institucional",
   description: "Sistema de acervo digital de TCCs (busca, visualização e gestão).",
   icons: {
     icon: "/favicon.ico",
